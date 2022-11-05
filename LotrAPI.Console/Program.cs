0cf4025633cfba0d85c89592c16a7fc379523f2b
kaposke <guilhermebassa@gmail.com>
2022-11-05 15:45:06 -0300

    Add console project for tinkering

---
+++ LotrAPI.Console/Program.cs
@@ -0,0 +1,8 @@
+﻿using LotrAPI.SDK;
+using LotrAPI.SDK.Options;
+using LotrAPI.SDK.Options.Filters;
+
+var client = new LotrClient("-qPsGZOoXUn89qgfoRaC");
+
+var movies = await client.GetMovies();
+Console.WriteLine(movies);
